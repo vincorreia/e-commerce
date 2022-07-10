f39e1a8666c264142d3333c6712dd233fae79a03
vincorreia <vcorreiaes@gmail.com>
2022-07-10 21:02:11 +0200

    Create dummy signup and Login buttons for the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Home(){
-    return <div className="sectionContainer"></div>
+    return (
+        <div className="sectionContainer row-center">
+            <div className="mainText col-center">
+                <h1>Welcome to Watches</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Maecenas aliquam condimentum malesuada. </p>
+                <div className="row-center">
+                    <button className="primary">Sign Up!</button>
+                    <button className="dark">Login</button>
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Nav(){
     return (
-    <nav className="space-around nav">
-            <div className="center">
+    <nav className="row-space-around nav">
+            <div className="row-center">
                 <a className="brand" href="#">Watches</a>
             </div>
-            <div className="center profile-section">
+            <div className="row-center profile-section">
                 <a href="#">SEARCH</a>
                 <a href="#">PROFILE</a>
                 <a href="#">CART</a>
