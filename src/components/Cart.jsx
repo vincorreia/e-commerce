650fe57b3d939c09666237ccd51d07e2ef1e437f
vincorreia <vcorreiaes@gmail.com>
2022-07-12 20:21:39 +0200

    get biel commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useCart } from "../context/CartContext";
+import NotFound from "./NotFound";
+
 
 export default function Cart(){
-    return (
-    <div className="sectionContainer">
 
+    const cart = useCart()
+    const cartItems = Object.entries(cart)
+    return (
+    <div className="sectionContainer flex-row center">
+        <div className="flex-col cart-wrapper">
+            <div className="cart-products flex-col">
+                {cartItems.length > 0 ? cartItems.map(item => {return <h1 className="cart-item">{item[1].name}</h1>}) : <NotFound page="cart" />}
+            </div>
+            <div className="checkout-details">
+            </div>
+        </div>
     </div>
     )
 }
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-export default function NotFound(){
-    return <div className="not-found">
+export default function NotFound(props){
+    const page = props.page;
+
+    return <div className={"not-found " + page}>
          <h1>No results found 😔</h1>
     </div>
 }
